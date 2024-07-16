9175bae34582ff9a3905f063a6dd8ba938be387a
ninanee <ni.yun@outlook.com>
2024-07-16 15:01:50 -0400

    Initial Commit

---
+++ SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by Yun Ni on 2024-07-16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
